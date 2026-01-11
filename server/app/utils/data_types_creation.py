6f20171b6bc90ea06f01428e98bb8df740030b5a
bergesedavide <davide.bergese07@gmail.com>
2026-01-11 11:52:41 +0100

    Creata la classe

--- server/app/utils/data_types_creation.py
+++ server/app/utils/data_types_creation.py
@@ -1,41 +1,50 @@
 from app.config import DataTypes
 import ast
 
-def literal_eval(string: str):
-    return ast.literal_eval(string)
-
-def todict(key_list: list, value_list: list, value_type: str = DataTypes.STRING.value):
-
-    type_map = {
-        DataTypes.STRING.value: str,
-        DataTypes.INTEGER.value: int,
-        DataTypes.FLOAT.value: float,
-        DataTypes.BOOLEAN.value: bool
-    }
-
-    value_expected_type = type_map[value_type]
-
-    new_key_list = []
-    for k in key_list:
-        if type(k) is type_map[DataTypes.BOOLEAN.value]:
-            raise TypeError(f"{k} non può essere un bool")
-
-        if type(k) is not type_map[DataTypes.STRING.value]:
-            k = str(k)
-            new_key_list.append(k)
-        else:
-            new_key_list.append(k)
+class DataManipulation:
+    def __init__(self):
+        pass
     
-    new_value_list = []
-    for v in value_list:
-        if type(v) is not value_expected_type:
-            if value_expected_type == type_map[DataTypes.STRING.value]:
-                v = str(v)
-                new_value_list.append(v)
+    def literal_eval(self, value: str):
+        return ast.literal_eval(value)
+
+    def todict(self, key_list: list, value_list: list, value_type: str = DataTypes.STRING.value):
+
+        type_map = {
+            DataTypes.STRING.value: str,
+            DataTypes.INTEGER.value: int,
+            DataTypes.FLOAT.value: float,
+            DataTypes.BOOLEAN.value: bool
+        }
+
+        value_expected_type = type_map[value_type]
+
+        new_key_list = []
+        for k in key_list:
+            if type(k) is type_map[DataTypes.BOOLEAN.value]:
+                raise TypeError(f"{k} non può essere un bool")
+
+            if type(k) is not type_map[DataTypes.STRING.value]:
+                k = str(k)
+                new_key_list.append(k)
             else:
-                raise TypeError(f"Valore non valido: {v}")
-        else:
-            new_value_list.append(v)
+                new_key_list.append(k)
         
-    result_dict = dict(zip(new_key_list, new_value_list))
-    return result_dict
+        new_value_list = []
+        for v in value_list:
+            if type(v) is not value_expected_type:
+                if value_expected_type == type_map[DataTypes.STRING.value]:
+                    v = str(v)
+                    new_value_list.append(v)
+                else:
+                    raise TypeError(f"Valore non valido: {v}")
+            else:
+                new_value_list.append(v)
+            
+        result_dict = dict(zip(new_key_list, new_value_list))
+        return result_dict
+
+    def tolist(self, value: str):
+        value_list = list(value)
+
+        return value_list
